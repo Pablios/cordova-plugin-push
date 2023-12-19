f6c13116d5b5f1748f837356d23dbbcc582878cf
Pablo Guilherme <99377068+Pablios@users.noreply.github.com>
2023-12-19 17:54:29 -0300

    Fix: ios notification

--- src/ios/AppDelegate+notification.m
+++ src/ios/AppDelegate+notification.m
@@ -223,14 +223,7 @@ didReceiveNotificationResponse:(UNNotificationResponse *)response
         case UIApplicationStateInactive:
         {
             NSLog(@"coldstart");
-            
-            if([response.actionIdentifier rangeOfString:@"UNNotificationDefaultActionIdentifier"].location == NSNotFound) {
-                self.launchNotification = userInfo;
-            }
-            else {
-                self.launchNotification = response.notification.request.content.userInfo;
-            }
-            
+            self.launchNotification = response.notification.request.content.userInfo;
             self.coldstart = [NSNumber numberWithBool:YES];
             break;
         }
